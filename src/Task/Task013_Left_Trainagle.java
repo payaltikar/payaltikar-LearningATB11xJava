1c86ddaa798dc3d3f7c608c99329511aed36cf92
Tikar <Payal29302@gmail.com>
2025-04-12 16:32:06 +0530

    12 april commmit

--- src/Task/Task013_Left_Trainagle.java
+++ src/Task/Task013_Left_Trainagle.java
@@ -1,4 +1,13 @@
 package Task;
 
 public class Task013_Left_Trainagle {
+    public static void main(String[] args) {
+        int n=3;
+        for(int i=3;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println( );
+        }
+    }
 }
--- src/Task/Task016_2n_Large_Array.java
+++ src/Task/Task016_2n_Large_Array.java
@@ -1,4 +1,16 @@
 package Task;
+import java.util.*;
 
 public class Task016_2n_Large_Array {
+    public static void main(String[] args) {
+        int[] Marks={90,80,98,96,75};
+        Arrays.sort(Marks);
+        for(int i=0;i<Marks.length;i++){
+            System.out.println(Marks[i]);
+        }
+
+        System.out.println("______");
+        System.out.println(Marks[Marks.length-2]);
+    }
+
 }
